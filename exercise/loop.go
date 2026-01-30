39b2408eb4a825d1b6d2ffa70c246403be25a6bd
Cristiano Aquino <cristiano.aquino@grupoavenida.com.br>
2026-01-29 22:48:58 -0300

    loop class

--- exercise/loop.go
+++ exercise/loop.go
@@ -3,7 +3,11 @@ import (
 	"fmt"
 )
 func main () {
-	for x := 0; x < 10; x++ {
-		fmt.Println(" ", x)
+	for horas := 0; horas <= 12; horas++ {
+		fmt.Println("Hora:", horas)
+		for minutos := 0; minutos <= 60; minutos++{
+			fmt.Println(" ", minutos)
+		}
+		fmt.Println("\n")
 	}
 }
