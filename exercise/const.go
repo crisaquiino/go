2df5969c7153d89c48789aa01fb0345eef65bdec
Cristiano Aquino <cristiano.aquino@grupoavenida.com.br>
2026-02-01 09:27:30 -0300

    exercicio constantes

---
+++ exercise/const.go
@@ -0,0 +1,22 @@
+package main
+import (
+	"fmt"
+)
+
+func main(){
+	fmt.Println("hello")
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// constantes sao valores imutaveis, por exemplo valores de banco de dados, valores que nao mudam ao longo do tempo
