e7381431f29cd7a63f8c81a810782c395c5a7c4f
Cristiano Aquino <cristiano.aquino@grupoavenida.com.br>
2026-02-04 21:48:01 -0300

    switch practice

---
+++ exercise/switch.go
@@ -0,0 +1,17 @@
+package main
+import (
+	"fmt"
+)
+
+func main (){
+	x := 5
+
+	switch {
+	case x == 5:
+		fmt.Println("x = 5 primeiro")
+	case x == 5:
+		fmt.Println("x e igual a cinco")
+	case x == 5:
+		fmt.Println("menor que 5")
+	}
+}
