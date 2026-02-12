12cc01744fc9308bf66e137259b72994d5f8d11b
Cristiano Aquino <cristiano.aquino@grupoavenida.com.br>
2026-02-11 23:00:08 -0300

    append

---
+++ exercise/slice/slice_array.go
@@ -0,0 +1,14 @@
+package main
+import (
+	"fmt"
+)
+
+func main (){
+	slice := make([]int, 5 ,10)
+	slice[0], slice[1], slice[2] = 1, 2, 3
+	slice = append(slice, 6)
+	slice = append(slice, 7)
+	fmt.Println(slice, len(slice), cap(slice))
+	slice = append(slice , 10)
+	fmt.Println(slice, len(slice), cap(slice))
+}
